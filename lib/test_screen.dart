7a4e458bd96e908668326fa09650a27f68697613
Hasnain-Mirrani07 <122273493+Hasnain-Mirrani-Tecrux@users.noreply.github.com>
2023-01-11 23:50:35 +0500

    .

---
+++ lib/test_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatefulWidget {
+  const TestScreen({super.key});
+
+  @override
+  State<TestScreen> createState() => _TextScreenState();
+}
+
+class _TextScreenState extends State<TestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              color: Colors.green,
+              child: const Text("container 1"),
+            ),
+          ),
+          Wrap(children: [
+            //  fit: FlexFit.loose,
+            Container(
+              // height: 100,
+              // width: 100,
+              color: Colors.yellow,
+              child: const Text(
+                  "containercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainer 2"),
+            ),
+            const Text(
+                "containercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainer 2"),
+            const Text(
+                "containercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainercontainer 2"),
+          ]),
+          Container(
+            height: 500,
+            width: 300,
+            color: Colors.red,
+            child: const Text("container 3"),
+          ),
+        ],
+      ),
+    );
+  }
+}
